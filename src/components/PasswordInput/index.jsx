224009c0659a30e2a8ccfb0da85984f56001645e
microlws <microlws@gmail.com>
2018-02-12 11:52:57 +0100

    made password input

---
+++ src/components/PasswordInput/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './index.scss';
+
+
+const PasswordInput = () => (
+  <input type='password' className='PasswordInput' />
+);
+
+export default PasswordInput;
--- src/containers/Content/index.jsx
+++ src/containers/Content/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import LoginInput from 'components/LoginInput';
+import PasswordInput from 'components/PasswordInput';
 
 import './index.scss';
 
@@ -8,6 +9,9 @@ const Content = () => (
     <div className='Content__inner'>
       <div className='Content__input'>
         <LoginInput />
+      </div>
+      <div className='Content__password'>
+        <PasswordInput />
       </div>
     </div>
   </div>
